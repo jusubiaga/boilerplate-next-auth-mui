4517ee2aa7a49f67e55e8216d3542c41ed9f1294
Juan Usubiaga <juan.usubiaga@bitlogic.io>
2024-03-27 20:41:13 -0300

    intial commit

---
+++ README.md
@@ -0,0 +1,64 @@
+# Boileplate Next, Auth, MUI
+
+Key Features:
+
+- 🔐 Next-auth v5 (Auth.js)
+- 🚀 Next.js 14 with server actions
+- 🔑 Credentials Provider
+- 🌐 OAuth Provider (Social login with Google & GitHub)
+- 🔒 Forgot password functionality
+- ✉️ Email verification
+- 📱 Two factor verification
+- 👥 User roles (Admin & User)
+- 🔓 Login component (Opens in redirect or modal)
+- 📝 Register component
+- 🤔 Forgot password component
+- ✅ Verification component
+- ⚠️ Error component
+- 🔘 Login button
+- 🚪 Logout button
+- 🚧 Role Gate
+
+### Prerequisites
+
+**Node version 18.7.x**
+
+### Cloning the repository
+
+### Install packages
+
+```shell
+npm i
+```
+
+### Setup .env file
+
+```js
+DATABASE_URL=
+DIRECT_URL=
+
+AUTH_SECRET=
+
+GITHUB_CLIENT_ID=
+GITHUB_CLIENT_SECRET=
+
+GOOGLE_CLIENT_ID=
+GOOGLE_CLIENT_SECRET=
+
+RESEND_API_KEY=
+
+NEXT_PUBLIC_APP_URL=
+```
+
+### Setup Prisma
+
+```shell
+npx prisma generate
+npx prisma db push
+```
+
+### Start the app
+
+```shell
+npm run dev
+```
---
+++ app/(protected)/company/page.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { CirclePlus, Megaphone, ChevronRight } from "lucide-react";
+
+function companyPage() {
+  return (
+    <div className="w-[97%]">
+      <header className="flex mb-4 justify-between items-center">
+        <h1>Recent Company</h1>
+        <div className="flex items-center">
+          <Button variant={"outline"}>
+            All Company <ChevronRight />
+          </Button>
+        </div>
+      </header>
+
+      <div className="flex gap-4">
+        <Card className="w-[150px] h-[150px] cursor-pointer">
+          <CardContent className="flex h-full w-full flex-col gap-y-5 items-center justify-center p-0 m-0">
+            {/* <Button variant="ghost" size="icon"> */}
+            <CirclePlus size={40} absoluteStrokeWidth />
+            {/* </Button> */}
+            <p>Create Company</p>
+          </CardContent>
+        </Card>
+        <Card className="w-[150px] h-[150px]">
+          <CardContent className="flex h-full w-full flex-col gap-y-5 items-center justify-center p-0 m-0">
+            {/* <Button variant="ghost" size="icon"> */}
+            <Megaphone size={40} absoluteStrokeWidth />
+            {/* </Button> */}
+            <p>New Company</p>
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  );
+}
+
+export default companyPage;
